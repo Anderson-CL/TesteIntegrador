513a191349faae4cfae3d3c23980fb3c7228224d
ANDERSON PEREIRA DOS SANTOS <anderson.psantos32@senacsp.edu.br>
2026-03-10 21:08:20 -0300

    Teste1

---
+++ IntegradorV1/IntegradorV1/TelaPrincipal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntegradorV1
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void caixaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void estoqueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
