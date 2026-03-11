d259d9f4c3ff00098101612ab85ada06fcd2cc0d
VictorDennisSA <victor.zika22@hotmail.com>
2026-03-11 18:00:34 -0300

    Criando o Caixa

---
+++ IntegradorV1/Caixa/Caixa.cs
@@ -0,0 +1,15 @@
+namespace Caixa
+{
+    public partial class Caixa : Form
+    {
+        public Caixa()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ IntegradorV1/IntegradorV1/Caixa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegradorV1
+{
+    internal class Caixa
+    {
+    }
+}
