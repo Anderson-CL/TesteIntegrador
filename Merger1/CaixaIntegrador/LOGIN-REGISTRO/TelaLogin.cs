24d08bfbd851c2dcde2291d2458ff876cdb6b306
VictorDennisSA <victor.zika22@hotmail.com>
2026-04-08 03:59:05 -0300

    aaaaaaa

---
+++ Merger1/CaixaIntegrador/LOGIN-REGISTRO/TelaLogin.cs
@@ -0,0 +1,137 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaIntegrador.LOGIN_REGISTRO
+{
+    public partial class TelaLogin : Form
+    {
+        private bool MostrarSenha = false;
+
+        private MySqlConnection Conexao;
+        public TelaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            if (txtLogin.Text == "" || txtSenha.Text == "")
+            {
+                MessageBox.Show("Por favor, Preencha todos os campos!", "Mensagem de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    string data_source = "datasource=localhost;username=root;password=;database=bd_adega";
+
+                    using (Conexao = new MySqlConnection(data_source))
+                    {
+                        Conexao.Open();
+
+                        string selectData = "SELECT * FROM usuarios WHERE login = @login AND senha = @senha";
+
+                        using (MySqlCommand comando = new MySqlCommand(selectData, Conexao))
+                        {
+                            comando.Parameters.AddWithValue("@login", txtLogin.Text.Trim());
+
+
+                            string senhaCriptografada =
+                                    Criptografia.CriptografarSenha(txtSenha.Text.Trim());
+
+
+                            comando.Parameters.AddWithValue("@senha", senhaCriptografada);
+
+                            MySqlDataAdapter adaptacao = new MySqlDataAdapter(comando);
+                            DataTable table = new DataTable();
+                            adaptacao.Fill(table);
+
+                            if (table.Rows.Count > 0)
+                            {
+                                MessageBox.Show("Login realizado com sucesso!", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                this.Hide();
+                                Form1 Caixa = new Form1();
+                                Caixa.ShowDialog();
+
+                                Application.Exit();
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Usuário ou Senha incorretos", "Mensagem de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro na conexão ou consulta: " + ex.Message, "Mensagem de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblRegistro_Click(object sender, EventArgs e)
+        {
+
+            this.Hide();
+            TelaRegistro RegistroForm = new TelaRegistro();
+            RegistroForm.ShowDialog();
+
+            this.Show();
+
+        }
+
+        private void TelaLogin_Load(object sender, EventArgs e)
+        {
+
+
+            txtSenha.UseSystemPasswordChar = true;
+
+            MostrarOcultar.Image = Properties.Resources.visivel;
+            MostrarOcultar.Visible = false;
+
+
+
+        }
+
+        private void MostrarOcultar_Click(object sender, EventArgs e)
+        {
+
+            {
+                MostrarSenha = !MostrarSenha;
+
+                txtSenha.UseSystemPasswordChar = !MostrarSenha;
+
+                MostrarOcultar.Image = MostrarSenha
+                    ? Properties.Resources.invisivel
+                    : Properties.Resources.visivel;
+            }
+
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+            MostrarOcultar.Visible = txtSenha.TextLength > 0;
+            if (txtSenha.TextLength == 0)
+            {
+                MostrarSenha = false;
+                txtSenha.UseSystemPasswordChar = true;
+                MostrarOcultar.Image = Properties.Resources.visivel;
+            }
+        }
+    }
+}
---
+++ Merger1/CaixaIntegrador/LOGIN-REGISTRO/TelaRegistro.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace CaixaIntegrador.LOGIN_REGISTRO
+{
+    partial class TelaRegistro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaRegistro));
+            panel1 = new Panel();
+            MostrarOcultar2 = new PictureBox();
+            MostrarOcultar1 = new PictureBox();
+            txtRegistroSenha2 = new TextBox();
+            pictureBox4 = new PictureBox();
+            btnRegistro = new Button();
+            txtRegistroSenha1 = new TextBox();
+            txtRegistroLogin = new TextBox();
+            lblRegistro = new Label();
+            lblEsqueceu = new Label();
+            label1 = new Label();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)MostrarOcultar2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)MostrarOcultar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoSize = true;
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(MostrarOcultar2);
+            panel1.Controls.Add(MostrarOcultar1);
+            panel1.Controls.Add(txtRegistroSenha2);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(btnRegistro);
+            panel1.Controls.Add(txtRegistroSenha1);
+            panel1.Controls.Add(txtRegistroLogin);
+            panel1.Controls.Add(lblRegistro);
+            panel1.Controls.Add(lblEsqueceu);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(245, 103);
+            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(457, 380);
+            panel1.TabIndex = 0;
+            // 
+            // MostrarOcultar2
+            // 
+            MostrarOcultar2.Cursor = Cursors.Hand;
+            MostrarOcultar2.Image = Properties.Resources.invisivel;
+            MostrarOcultar2.Location = new Point(299, 204);
+            MostrarOcultar2.Name = "MostrarOcultar2";
+            MostrarOcultar2.Size = new Size(20, 23);
+            MostrarOcultar2.SizeMode = PictureBoxSizeMode.Zoom;
+            MostrarOcultar2.TabIndex = 11;
+            MostrarOcultar2.TabStop = false;
+            MostrarOcultar2.Click += MostrarOcultar2_Click;
+            // 
+            // MostrarOcultar1
+            // 
+            MostrarOcultar1.Cursor = Cursors.Hand;
+            MostrarOcultar1.Image = Properties.Resources.invisivel;
+            MostrarOcultar1.Location = new Point(299, 155);
+            MostrarOcultar1.Name = "MostrarOcultar1";
+            MostrarOcultar1.Size = new Size(20, 23);
+            MostrarOcultar1.SizeMode = PictureBoxSizeMode.Zoom;
+            MostrarOcultar1.TabIndex = 10;
+            MostrarOcultar1.TabStop = false;
+            MostrarOcultar1.Click += MostrarOcultar1_Click;
+            // 
+            // txtRegistroSenha2
+            // 
+            txtRegistroSenha2.Cursor = Cursors.IBeam;
+            txtRegistroSenha2.Location = new Point(183, 204);
+            txtRegistroSenha2.Margin = new Padding(3, 2, 3, 2);
+            txtRegistroSenha2.Name = "txtRegistroSenha2";
+            txtRegistroSenha2.Size = new Size(110, 23);
+            txtRegistroSenha2.TabIndex = 3;
+            txtRegistroSenha2.TextChanged += txtRegistroSenha2_TextChanged;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(121, 204);
+            pictureBox4.Margin = new Padding(3, 2, 3, 2);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(25, 23);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // btnRegistro
+            // 
+            btnRegistro.Cursor = Cursors.Hand;
+            btnRegistro.Location = new Point(193, 244);
+            btnRegistro.Margin = new Padding(3, 2, 3, 2);
+            btnRegistro.Name = "btnRegistro";
+            btnRegistro.Size = new Size(82, 22);
+            btnRegistro.TabIndex = 4;
+            btnRegistro.Text = "REGISTRAR";
+            btnRegistro.UseVisualStyleBackColor = true;
+            btnRegistro.Click += btnRegistro_Click;
+            // 
+            // txtRegistroSenha1
+            // 
+            txtRegistroSenha1.Cursor = Cursors.IBeam;
+            txtRegistroSenha1.Location = new Point(183, 155);
+            txtRegistroSenha1.Margin = new Padding(3, 2, 3, 2);
+            txtRegistroSenha1.Name = "txtRegistroSenha1";
+            txtRegistroSenha1.Size = new Size(110, 23);
+            txtRegistroSenha1.TabIndex = 2;
+            txtRegistroSenha1.TextChanged += txtRegistroSenha1_TextChanged;
+            // 
+            // txtRegistroLogin
+            // 
+            txtRegistroLogin.Cursor = Cursors.IBeam;
+            txtRegistroLogin.Location = new Point(183, 111);
+            txtRegistroLogin.Margin = new Padding(3, 2, 3, 2);
+            txtRegistroLogin.Name = "txtRegistroLogin";
+            txtRegistroLogin.Size = new Size(110, 23);
+            txtRegistroLogin.TabIndex = 1;
+            // 
+            // lblRegistro
+            // 
+            lblRegistro.AutoSize = true;
+            lblRegistro.Cursor = Cursors.Hand;
+            lblRegistro.ForeColor = Color.RoyalBlue;
+            lblRegistro.Location = new Point(270, 292);
+            lblRegistro.Name = "lblRegistro";
+            lblRegistro.Size = new Size(38, 15);
+            lblRegistro.TabIndex = 5;
+            lblRegistro.Text = "Entrar";
+            lblRegistro.Click += lblRegistro_Click;
+            // 
+            // lblEsqueceu
+            // 
+            lblEsqueceu.AutoSize = true;
+            lblEsqueceu.Location = new Point(121, 292);
+            lblEsqueceu.Name = "lblEsqueceu";
+            lblEsqueceu.Size = new Size(106, 15);
+            lblEsqueceu.TabIndex = 6;
+            lblEsqueceu.Text = "Já tem uma conta?";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(183, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 15);
+            label1.TabIndex = 0;
+            label1.Text = "TELA DE REGISTRO";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(121, 155);
+            pictureBox3.Margin = new Padding(3, 2, 3, 2);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(25, 23);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(121, 111);
+            pictureBox2.Margin = new Padding(3, 2, 3, 2);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(25, 23);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(209, 16);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(55, 52);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // TelaRegistro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(949, 594);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "TelaRegistro";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "8";
+            Load += TelaRegistro_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)MostrarOcultar2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)MostrarOcultar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox txtRegistroSenha2;
+        private PictureBox pictureBox4;
+        private Button btnRegistro;
+        private TextBox txtRegistroSenha1;
+        private TextBox txtRegistroLogin;
+        private Label lblRegistro;
+        private Label lblEsqueceu;
+        private Label label1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private PictureBox MostrarOcultar2;
+        private PictureBox MostrarOcultar1;
+    }
+}
